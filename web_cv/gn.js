ebe1f09aa47c4da664b3c825b623b6f6d989d447
yrafas <102044986+yrafas@users.noreply.github.com>
2022-04-12 21:29:40 +0200

    nv

---
+++ web_cv/gn.js
@@ -0,0 +1,8 @@
+var hoverskill=document.getElementById("hoverskill");
+			hoverskill.onmouseover = function(e) {
+				// Evento cuando mouse pasa por encima
+				document.getElementById("aebar").style.width= "70%";
+                document.getElementById("psbar").style.width= "60%";
+                document.getElementById("prbar").style.width= "80%";
+                document.getElementById("dvbar").style.width= "50%";
+            }
